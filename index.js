338377963875ad92c1b1a4f9e4e279a7073c5d1f
Amr <amr70210@gamil.com>
2023-10-18 18:52:49 +0300

    pro

---
+++ index.js
@@ -0,0 +1,28 @@
+$(()=>{
+    $('.burger').on('click',()=>{
+        $('nav.mobile').removeClass('hide')
+        $('nav.mobile').addClass('flex')
+    })
+
+    $('.close').on('click',()=>{
+        $('nav.mobile').addClass('hide')
+    })
+
+    // pricing interactions
+    $('.toggle').on('click',()=>{
+        $('.toggle > span').toggleClass('right')
+        $('.monthly, .yearly').toggleClass('selected')
+        $('.month-plan, .year-plan').toggleClass('hide')
+    })
+
+    // Fixed Nav onScroll.
+    $(window).scroll(()=>{
+        let height = $(this).scrollTop();
+        if(height > 100){
+            $('.hero header').addClass('fixed-header');
+        }
+        else{
+            $('.hero header').removeClass('fixed-header');
+        }
+    })
+})
